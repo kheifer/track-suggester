ecceae212d5a55083bfeee71f4c095ab8fd23d38
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-07 15:10:22 -0700

    add img folder and removed jumbotron

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
+  $("#main").click(function(){
+    $("#quiz").fadeIn(200);
+    $("#main").hide();
+});
+
+
   var favoriteColor = $("#color").val();
-  
+  var userName = $("input#name").val();
 
 
 
 
 
+
+
+
+  $(".username").append(userName);
+  $(".result").append(languageresult);
 
    event.preventDefault();
 
