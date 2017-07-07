6359666c4fbe2f5ea9ad61b8628618e8610a7874
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-07 16:28:34 -0700

    add .js functionality

--- js/scripts.js
+++ js/scripts.js
@@ -2,23 +2,59 @@ $(document).ready(function(){
   $("#main").click(function(){
     $("#quiz").fadeIn(200);
     $("#main").hide();
+    $(".results").hide();
     $("#sidebar").hide();
-});
+  });
+
 
 
   var favoriteColor = $("#color").val();
   var userName = $("input#name").val();
+  var question2 =$("input:radio[name=quest2]:checked").val();
+  var question3 =$("input:radio[name=quest3]:checked").val();
+  var question4 =$("input:radio[name=quest4]:checked").val();
+  var question5 =$("input:radio[name=quest5]:checked").val();
 
 
 
+
+
+  $("#quizclick").click(function(){
+
+  if (question2 === "established") {
+    if (question3 === "interesting") {
+      //Java
+      $("#java").delay(300).show( "slide", {direction: "right"}, 200);
+    } else {
+      //C#
+      $("#c-sharp").delay(300).show( "slide", {direction: "right"}, 200);
+
+    }
+
+  } else if(question2 === "startup") {
+    if (question3 === "interesting") {
+      //RUBY
+      $("#java").delay(300).show( "slide", {direction: "right"}, 200);
+    } else{
+      //PHP
+      $("#php").delay(300).show( "slide", {direction: "right"}, 200);
+    }
+  } else {
+    if (question3 === "interesting") {
+      //RUBY
+      $("#ruby").delay(300).show( "slide", {direction: "right"}, 200);
+    } else {
+      //CSS
+      $("#css").delay(300).show( "slide", {direction: "right"}, 200);
+    }
+  }
+});
 
 
 
 
 
   $(".username").append(userName);
-  $(".result").append(languageresult);
-
    event.preventDefault();
 
 });
