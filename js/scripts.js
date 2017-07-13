3cf21746b5d37ef823293d09409542d0f8faaa9a
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-12 18:58:08 -0700

    add a remote jquery file

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,71 @@
-$(document).ready(function(){
-  $("#java").hide();
-  $("#c-sharp").hide();
-  $("#php").hide();
-  $("#ruby").hide();
-  $("#css").hide();
-
-  $("#main-butn").click(function(){
-    $("#quiz").fadeIn(200);
-    $("#main").hide();
-    $("#sidebar").hide();
-
-
-  });
-
-
-
+$(document).ready(function() {
   var favoriteColor = $("#color").val();
   var userName = $("input#name").val();
-  var question2 =$("input:radio[name=quest2]:checked").val();
-  var question3 =$("input:radio[name=quest3]:checked").val();
-  var question4 =$("input:radio[name=quest4]:checked").val();
-  var question5 =$("input:radio[name=quest5]:checked").val();
-
-  $(".username").append(userName);
-
-
-
-
-  $("#form").submit(function(){
+  var question2 = $("input:radio[name=quest2]:checked").val();
+  var question3 = $("input:radio[name=quest3]:checked").val();
+  var question4 = $("input:radio[name=quest4]:checked").val();
+  var question5 = $("input:radio[name=quest5]:checked").val();
+  $("#track-sug").submit(function(event) {
+    event.preventDefault();
+    $(".username").text(userName);
     if (question2 === "established") {
       if (question3 === "interesting") {
-        //Java
         $("#quiz").fadeOut(200);
-        $("#java").show( "slide", {direction: "right"}, 200);
+        $("#main").hide();
+        $("#sidebar").hide();
+        alert("Why isn't this working?");
+        $("#java").show("slide", {
+          direction: "right"
+        }, 200);
       } else {
-        //C#
         $("#quiz").fadeOut(200);
-        $("#c-sharp").show( "slide", {direction: "right"}, 200);
-
+        $("#main").hide();
+        $("#sidebar").hide();
+        $("#c-sharp").show("slide", {
+          direction: "right"
+        }, 200);
       }
-
-    } else if(question2 === "startup") {
+    } else if (question2 === "startup") {
       if (question3 === "interesting") {
-        //RUBY
         $("#quiz").fadeOut(200);
-        $("#ruby").show( "slide", {direction: "right"}, 200);
-      } else{
-        //PHP
+        $("#main").hide();
+        $("#sidebar").hide();
+        $("#ruby").show("slide", {
+          direction: "right"
+        }, 200);
+        alert("Why isn't this working?");
+      } else {
         $("#quiz").fadeOut(200);
-        $("#php").show( "slide", {direction: "right"}, 200);
+        $("#main").hide();
+        $("#sidebar").hide();
+        $("#php").show("slide", {
+          direction: "right"
+        }, 200);
       }
     } else {
       if (question3 === "interesting") {
-        //RUBY
         $("#quiz").fadeOut(200);
-        $("#ruby").show( "slide", {direction: "right"}, 200);
+        $("#main").hide();
+        $("#sidebar").hide();
+        $("#ruby").show("slide", {
+          direction: "right"
+        }, 200);
       } else {
-        //CSS
         $("#quiz").fadeOut(200);
-        $("#css").show( "slide", {direction: "right"}, 200);
+        $("#main").hide();
+        $("#sidebar").hide();
+        $("#css").show("slide", {
+          direction: "right"
+        }, 200);
       }
     }
-    $("#java").hide();
-    $("#c-sharp").hide();
-    $("#php").hide();
-    $("#ruby").hide();
-    $("#css").hide();
-
-});
-
+  });
+  $("#main-butn").click(function() {
+    $("#quiz").fadeIn(200);
+    $("#main").hide();
+    $("#sidebar").hide();
+  });
 
 
-
-  $(".username").append(userName);
-
-   event.preventDefault();
 
 });
